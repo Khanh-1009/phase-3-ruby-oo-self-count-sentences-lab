5efc8b21e35a5d6f38cfce666a3ccfb4872a5b24
Khanh Tran <katetrannbkhanh@gmail.com>
2023-05-05 18:33:50 -0700

    commit tree 9c19fb827758d28f0fa41fad59dcd7977fa93980

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,36 @@
 require 'pry'
 
 class String
-
   def sentence?
-    
+    if self.end_with?(".")
+      true
+    else
+      false
+    end
   end
 
-  def question?
-
+  def question? 
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    sentence_squeeze = self.squeeze()
+    sentence_split = sentence_squeeze.split(%r{\s*})
+    period = sentence_split.count(".")
+    question = sentence_split.count("?")
+    exclamation = sentence_split.count("!")
+    period + question + exclamation
   end
 end
